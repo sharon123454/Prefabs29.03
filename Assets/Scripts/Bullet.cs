78f4eff96df4ca208ccae5f914a8de0bd74352bd
RoxasCodes <evicokazzz@gmail.com>
2023-04-19 18:58:52 +0300

    bullet push

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    PlayerController PlayerCon;
     Vector2 vector2;
     public float speed;
     private Rigidbody2D rb;
 
     private void Start()
     {
+        //Vector2 vc2 = new Vector2 (PlayerCon.moveDir.x, 0);
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.left * speed);
+        rb.AddForce(vc2 * speed);
     }
 }
