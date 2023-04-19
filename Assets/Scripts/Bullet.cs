e9c14f626f0472b5599e6b59650744ea02e1e07b
RoxasCodes <evicokazzz@gmail.com>
2023-04-19 18:46:24 +0300

    bullet push

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,6 +11,6 @@ public class Bullet : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.one * speed);
+        rb.AddForce(Vector2.left * speed);
     }
 }
