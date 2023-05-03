7f9e6609b8becb6ebe022f8e34d8a13537cb329d
Leeshap98 <1998lee@gmail.com>
2023-05-03 18:49:12 +0300

    Scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 10;
+    [SerializeField] float timeToIdle = 0.8f;
+    [SerializeField] SkeletonAnimation skeletonAnimation;
     
-    float horizontalInput;
     public Bullet bulletPref;
     public Transform shootingPoint;
+
+    private float horizontalInput;
     private Vector3 moveDir;
     private Quaternion flipped = new Quaternion(0, 180, 0, 0);
     private Quaternion regular = new Quaternion(0, 0, 0, 0);
-
 
     private void Update()
     {
         PlayerInput();
         PlayerMovement();
+        Shooting();
+    }
+
+    private void Shooting()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             var tempBullet = Instantiate(bulletPref, shootingPoint.position, transform.rotation);
             tempBullet.bulletAddforce(moveDir);
+            skeletonAnimation.AnimationName = "ATTACK";
+            StopCoroutine(ChangeToIdle());
+            StartCoroutine(ChangeToIdle());
         }
+        
+    }
+
+    private IEnumerator ChangeToIdle()
+    {
+        yield return new WaitForSeconds(timeToIdle);
+        skeletonAnimation.AnimationName = "IDLE";
     }
 
     void PlayerMovement()
@@ -35,7 +53,7 @@ public class PlayerController : MonoBehaviour
         {
             transform.rotation = flipped;
         }
-        else
+        else if (horizontalInput < 0)
             transform.rotation = regular;
     }
 
