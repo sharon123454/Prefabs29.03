903dadfabb212c6acaec9d9eb0ffa996faf934d1
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-05-03 19:06:37 +0300

    .

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,29 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-{
-    PlayerController PlayerCon;
+{ 
+    [SerializeField] private  float timeTillDeath = 5;
+
+    private Quaternion flipped = new Quaternion(0, 180, 0, 0);
+    private Quaternion regular = new Quaternion(0, 0, 0, 0);
     Vector2 vector2 = Vector2.right;
     public float speed;
     private Rigidbody2D rb;
+    float timer;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        tempTest();
+        rb.AddForce(vector2 * speed);
     }
-
 
     public void bulletAddforce(Vector2 vc2)
     {
         if (vc2 != Vector2.zero)
-        {
             vector2 = vc2;
-        }
+
+        timer = timeTillDeath;
+
+        if (vc2.normalized.x > 0)
+            transform.rotation = flipped;
+        else if (vc2.normalized.x < 0)
+            transform.rotation = regular;
     }
 
-    private void tempTest()
+    private void Update()
     {
-        rb.AddForce(vector2 * speed);
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+            Destroy(gameObject);
     }
 }
